2e10be309c55be305fc386000390aae1d9704180
marco <s105062638@m105.nthu.edu.tw>
2018-03-12 15:00:45 +0800

    modify script

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get install -y iperf screen
+
+sudo apt-get update && sudo apt-get install default-jre
+wget -O ReSafetyServer.jar http://mnet.cs.nthu.edu.tw/mnet_old/ReSafetyServer/ReSafetyServer.jar
+wget -O Setting http://mnet.cs.nthu.edu.tw/mnet_old/ReSafetyServer/Setting
+java -jar ReSafetyServer.jar
